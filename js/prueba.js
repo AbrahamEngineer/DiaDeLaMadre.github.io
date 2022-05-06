b74e238c3398e0be645dbd1b006e020dc525bf20
AbrahamEngineer <abrahamguillermoc8@gmail.com>
2022-05-06 12:04:29 -0500

    Imagenes agregadas

--- js/prueba.js
+++ js/prueba.js
@@ -25,7 +25,7 @@ var swiper = new Swiper(".mySwiper", {
             slidesPerView: 3,
             spaceBetween: 30,
             speed: 200,
-            
+
         },
 
         992: {
@@ -42,7 +42,7 @@ var swiper = new Swiper(".mySwiper", {
 
         0: {
             slidesPerView: 1,
-            spaceBetween: 30,
+            spaceBetween: 10,
             speed: 200,
         },
     },
@@ -52,3 +52,41 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: '.swiper-button-prev',
     },
 });
+
+window.addEventListener('load', (e) => {
+    eventosCarta();
+    pasarHojas();
+});
+
+
+function eventosCarta() {
+    const carta = document.getElementById('carta');
+    const btnCerrar = document.getElementById('btnCerrar');
+    const iconoCorazon = document.querySelector('.iconoCorazon');
+
+    if (screen.width > 768) {
+        carta.addEventListener('mouseover', (e) => {
+            carta.classList.add("hover");
+        });
+        carta.addEventListener('mouseout', (e) => {
+            carta.classList.remove("hover");
+        });
+    } else {
+        iconoCorazon.addEventListener('click', (e) => {
+            carta.classList.add("hover");
+        });
+
+        btnCerrar.addEventListener('click', (e) => {
+            carta.classList.remove("hover");
+        });
+    }
+}
+
+function pasarHojas() {
+    const hojas = document.querySelectorAll('.carta .contenido .posiciones #mensajes .cuaderno .hoja');
+    hojas.forEach(hoja => {
+        hoja.addEventListener('click', (e) => {
+            hoja.classList.toggle('pasar');
+        });
+    });
+}
